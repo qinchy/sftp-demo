ba18f6e5e59f6fee5f17c8a6ed0427f0589f251c
qinchy <qinchy@msn.com>
2018-07-25 21:42:25 +0800

    init

---
+++ src/main/java/com/qinchy/sftpdemo/TwoNumberAdder.java
@@ -0,0 +1,175 @@
+package com.qinchy.sftpdemo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Solution {
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        StringBuffer sb1 = new StringBuffer();
+//        StringBuffer sb2 = new StringBuffer();
+//        get(l1,sb1);
+//        get(l2,sb2);
+//
+//        StringBuffer reverse1 = sb1.reverse();
+//        StringBuffer reverse2 = sb2.reverse();
+//
+//        java.math.BigDecimal b1 = new java.math.BigDecimal(reverse1.toString());
+//        java.math.BigDecimal b2 = new java.math.BigDecimal(reverse2.toString());
+//
+//        java.math.BigDecimal sum = b1.add(b2);
+//        StringBuffer sss = new StringBuffer();
+//        sss.append(sum);
+//        String s = sss.reverse().toString();
+//        String[] split = s.split("");
+//        StringBuffer ssssss = new StringBuffer();
+//        for (String ss: split){
+//            ssssss.append(ss).append(",");
+//        }
+//
+//        String substring = ssssss.toString().substring(0, ssssss.length() - 1);
+//        return stringToListNode("["+substring+"]");
+//    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public StringBuffer get(ListNode ll,StringBuffer sb){
+        if (null == ll.next){
+            return sb.append(ll.val);
+        }else{
+             return get(ll.next,sb.append(ll.val));
+        }
+    }
+
+    public int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for (int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+}
+
+public class TwoNumberAdder {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for (int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode l1 = stringToListNode(line);
+            line = in.readLine();
+            ListNode l2 = stringToListNode(line);
+
+            ListNode ret = new Solution().addTwoNumbers(l1, l2);
+
+            String out = listNodeToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
