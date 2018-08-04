2f17579f204b083554a99784df2ff6e88f7dcbbd
qinchy <qinchy@msn.com>
2018-08-04 17:13:31 +0800

    init

--- src/main/java/com/qinchy/sftpdemo/FindMedianSortedArrays.java
+++ src/main/java/com/qinchy/sftpdemo/FindMedianSortedArrays.java
@@ -6,6 +6,28 @@ import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.util.Arrays;
 
+/**
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2 。
+ *
+ * 请找出这两个有序数组的中位数。要求算法的时间复杂度为 O(log (m+n)) 。
+ *
+ * 你可以假设 nums1 和 nums2 均不为空。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ *
+ * 中位数是 2.0
+ * 示例 2:
+ *
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ *
+ * 中位数是 (2 + 3)/2 = 2.5
+ */
 class Solution4 {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int total = nums1.length + nums2.length;
