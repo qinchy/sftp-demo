3cf263f76073ccedc850e623c3ba0e9b29cf374b
qinchy <qinchy@msn.com>
2018-08-04 17:12:19 +0800

    init

--- src/main/java/com/qinchy/sftpdemo/LongestPalindrome.java
+++ src/main/java/com/qinchy/sftpdemo/LongestPalindrome.java
@@ -4,6 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000。
+ *
+ * 示例 1：
+ *
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba"也是一个有效答案。
+ * 示例 2：
+ *
+ * 输入: "cbbd"
+ * 输出: "bb"
+ */
 class Solution5 {
     public String longestPalindrome(String s) {
       if (s == null || s.length() < 2) {
